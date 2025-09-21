50eabd97ecebd7a8ea4f072f893c54cfe5399772
warm bu <an.p6053@gmail.com>
2025-09-21 21:39:59 +0300

    Added advanced ARP Packets support

--- src/arp.rs
+++ src/arp.rs
@@ -1,21 +1,22 @@
 use crate::util::Packet;
-#[cfg(feature = "legacy-arp")]
+#[cfg(feature = "advanced-arp")]
 use crate::util::{HardwareAddress, ProtocolAddress};
-#[cfg(not(feature = "legacy-arp"))]
+#[cfg(not(feature = "advanced-arp"))]
 use {
     crate::util::MacAddress,
     std::net::Ipv4Addr
 };
 
+/// Just ARP Packet `operation` field, can be `Request` or `Reply`, or something else if you using other ARP types, such as Reverse ARP(RARP)
 #[derive(Debug, Clone)]
 pub enum ArpOperation {
     Request,
     Reply,
     RarpRequest,
     RarpReply,
-    #[cfg(feature = "legacy-arp")]
+    #[cfg(feature = "advanced-arp")]
     InArpRequest,
-    #[cfg(feature = "legacy-arp")]
+    #[cfg(feature = "advanced-arp")]
     InArpReply
 }
 impl ArpOperation {
@@ -25,13 +26,13 @@ impl ArpOperation {
             2 => Self::Reply,
             3 => Self::RarpRequest,
             4 => Self::RarpReply,
-            #[cfg(feature = "legacy-arp")]
+            #[cfg(feature = "advanced-arp")]
             8 => Self::InArpRequest,
-            #[cfg(feature = "legacy-arp")]
+            #[cfg(feature = "advanced-arp")]
             9 => Self::InArpReply,
-            #[cfg(feature = "legacy-arp")]
+            #[cfg(feature = "advanced-arp")]
             _ => panic!("Value can be only 1, 2, 3, 4, 8, 9!"),
-            #[cfg(not(feature = "legacy-arp"))]
+            #[cfg(not(feature = "advanced-arp"))]
             _ => panic!("Value can be only 1, 2, 3, 4!")
         }
     }
@@ -41,15 +42,16 @@ impl ArpOperation {
             Self::Reply => 2,
             Self::RarpRequest => 3,
             Self::RarpReply => 4,
-            #[cfg(feature = "legacy-arp")]
+            #[cfg(feature = "advanced-arp")]
             Self::InArpRequest => 8,
-            #[cfg(feature = "legacy-arp")]
+            #[cfg(feature = "advanced-arp")]
             Self::InArpReply => 9
         }
     }
 }
 
-#[cfg(feature = "legacy-arp")]
+/// Advanced ARP Packet, can resolve many legacy ARP Packets, such as AX25, ATM and other, due to the presence of vectors instead of ready-made structs like `Ipv4Addr` and `MacAddress`
+#[cfg(feature = "advanced-arp")]
 #[derive(Debug, Clone)]
 pub struct ArpPacket {
     pub hardware_type: u16,
@@ -57,15 +59,14 @@ pub struct ArpPacket {
     pub hardware_addr_len: u8,
     pub protocol_addr_len: u8,
     pub operation: ArpOperation,
-    pub sender_hardware_addr: HardwareAddress,
-    pub sender_protocol_addr: ProtocolAddress,
-    pub target_hardware_addr: HardwareAddress,
-    pub target_protocol_addr: ProtocolAddress,
+    pub sender_hardware_addr: Vec<u8>,
+    pub sender_protocol_addr: Vec<u8>,
+    pub target_hardware_addr: Vec<u8>,
+    pub target_protocol_addr: Vec<u8>
 }
-#[cfg(not(feature = "legacy-arp"))]
+/// Normal ARP Packet, containing sender and target MAC and IP Addresses, with `hardware_addr_len = 6`, `protocol_addr_len = 4`, `hardware_type = 1` and `protocol_type = 0x0800(2048)`
+#[cfg(not(feature = "advanced-arp"))]
 pub struct ArpPacket {
-    pub hardware_type: u16,
-    pub protocol_type: u16,
     pub operation: ArpOperation,
     pub sender_mac: MacAddress,
     pub sender_ip: Ipv4Addr,
@@ -74,7 +75,7 @@ pub struct ArpPacket {
 }
 impl ArpPacket {
     /// Constructs an empty `ArpPacket`
-    #[cfg(feature = "legacy-arp")]
+    #[cfg(feature = "advanced-arp")]
     pub fn new() -> Self {
         Self {
             hardware_type: 1,
@@ -82,18 +83,16 @@ impl ArpPacket {
             hardware_addr_len: 6,
             protocol_addr_len: 4,
             operation: ArpOperation::Request,
-            sender_hardware_addr: HardwareAddress::None,
-            sender_protocol_addr: ProtocolAddress::None,
-            target_hardware_addr: HardwareAddress::None,
-            target_protocol_addr: ProtocolAddress::None
+            sender_hardware_addr: Vec::new(),
+            sender_protocol_addr: Vec::new(),
+            target_hardware_addr: Vec::new(),
+            target_protocol_addr: Vec::new()
         }
     }
     /// Constructs an empty `ArpPacket`
-    #[cfg(not(feature = "legacy-arp"))]
+    #[cfg(not(feature = "advanced-arp"))]
     pub fn new() -> Self {
         Self {
-            hardware_type: 1,
-            protocol_type: 0x0800,
             operation: ArpOperation::Request,
             sender_mac: MacAddress::new(),
             sender_ip: Ipv4Addr::new(0, 0, 0, 0),
@@ -106,24 +105,31 @@ impl Packet for ArpPacket {
     /// Constructs `ArpPacket` from existing packet bytes
     fn from_bytes(bytes: &[u8]) -> Self {
         let mut packet = Self::new();
-        packet.hardware_type = u16::from_be_bytes([bytes[0], bytes[1]]);
-        packet.protocol_type = u16::from_be_bytes([bytes[2], bytes[3]]);
         packet.operation = ArpOperation::from_value(u16::from_be_bytes([bytes[6], bytes[7]]));
-        #[cfg(feature = "legacy-arp")]
+        #[cfg(feature = "advanced-arp")]
         {
+            packet.hardware_type = u16::from_be_bytes([bytes[0], bytes[1]]);
+            packet.protocol_type = u16::from_be_bytes([bytes[2], bytes[3]]);
             packet.hardware_addr_len = bytes[4];
             packet.protocol_addr_len = bytes[5];
-            unimplemented!();
-            // Parse hardware and protocol types here
-            // Parse addresses here
+            packet.sender_hardware_addr = bytes[8..8 + packet.hardware_addr_len].to_vec();
+            packet.sender_protocol_addr = bytes[8 + packet.hardware_addr_len..8 + packet.hardware_addr_len + packet.protocol_addr_len].to_vec();
+            packet.target_hardware_addr = bytes[8 + packet.hardware_addr_len + packet.protocol_addr_len..8 + 2 * packet.hardware_addr_len + packet.protocol_addr_len].to_vec();
+            packet.target_protocol_addr = bytes[8 + 2 * packet.hardware_addr_len + packet.protocol_addr_len..8 + 2 * packet.hardware_addr_len + 2 * packet.protocol_addr_len].to_vec();
         }
-        #[cfg(not(feature = "legacy-arp"))]
+        #[cfg(not(feature = "advanced-arp"))]
         {
+            if u16::from_be_bytes([bytes[0], bytes[1]]) != 1 {
+                panic!("Hardware type must be only 1, if you need to parse other hardware types, use 'advanced-arp' feature");
+            }
+            if u16::from_be_bytes([bytes[2], bytes[3]]) != 0x0800 {
+                panic!("Protocol type must be only 0x0800(2048), if you need to parse other protocol types, use 'advanced-arp' feature");
+            }
             if bytes[4] != 6 {
-                panic!("Hardware Address Length in normal ARP packet is equal to 6, use 'legacy-arp' feature to parse more ARP Packet types");
+                panic!("Hardware Address Length in normal ARP packet is equal to 6, use 'advanced-arp' feature to parse more ARP Packet types");
             }
             if bytes[5] != 4 {
-                panic!("Protocol Address Length in normal ARP packet is equal to 6, use 'legacy-arp' feature to parse more ARP Packet types");
+                panic!("Protocol Address Length in normal ARP packet is equal to 6, use 'advanced-arp' feature to parse more ARP Packet types");
             }
             packet.sender_mac = MacAddress::from_slice(&bytes[8..=13]);
             packet.sender_ip = Ipv4Addr::new(bytes[14], bytes[15], bytes[16], bytes[17]);
@@ -135,20 +141,25 @@ impl Packet for ArpPacket {
     /// Converting **full** packet to bytes
     /// Note that in context of `ArpPacket` methods `header_to_bytes()` and `to_bytes()` are equal, because ARP Packet doesn't have payload
     fn header_to_bytes(&self) -> Vec<u8> {
-        let mut packet = Vec::<u8>::with_capacity(8);
-        packet.append(&mut self.hardware_type.to_be_bytes().to_vec());
-        packet.append(&mut self.protocol_type.to_be_bytes().to_vec());
-        packet.push(0);
-        packet.push(0);
-        packet.append(&mut self.operation.to_value().to_be_bytes().to_vec());
-        #[cfg(feature = "legacy-arp")]
+        let mut packet = vec![0u8; 8];
+        packet[6..=7].copy_from_slice(&self.operation.to_value().to_be_bytes());
+        #[cfg(feature = "advanced-arp")]
         {
+            packet[0..=1].copy_from_slice(&self.hardware_type.to_be_bytes());
+            packet[2..=3].copy_from_slice(&self.protocol_type.to_be_bytes());
             packet[4] = self.hardware_addr_len;
             packet[5] = self.protocol_addr_len;
-            // Finish it
+            packet.append(&mut self.sender_hardware_addr.clone());
+            packet.append(&mut self.sender_protocol_addr.clone());
+            packet.append(&mut self.target_hardware_addr.clone());
+            packet.append(&mut self.target_protocol_addr.clone());
         }
-        #[cfg(not(feature = "legacy-arp"))]
+        #[cfg(not(feature = "advanced-arp"))]
         {
+            packet[0] = 0;
+            packet[1] = 1;
+            packet[2] = 8;
+            packet[3] = 0;
             packet[4] = 6;
             packet[5] = 4;
             packet.append(&mut self.sender_mac.to_bytes().to_vec());
--- src/util.rs
+++ src/util.rs
@@ -1,72 +1,34 @@
-use paste::paste;
-
-macro_rules! hardware_address {
-    ($name:ident, $desc:expr, $size:expr) => {
-        paste! {
-            #[doc = $desc]
-            #[derive(Debug, Clone, Copy)]
-            pub struct [<$name Address>] {
-                /// $name Address Data Bytes
-                pub bytes: [u8; $size]
-            }
-            impl [<$name Address>] {
-                pub fn new() -> Self {
-                    Self {
-                        bytes: [0u8; $size]
-                    }
-                }
-                pub fn from_slice(bytes: &[u8]) -> Self {
-                    if bytes.len() < $size {
-                        panic!("Bytes len must be $size!");
-                    }
-                    let mut new_bytes: [u8; $size] = [0; $size];
-                    new_bytes.copy_from_slice(bytes);
-                    Self {
-                        bytes: new_bytes
-                    }
-                }
-                pub fn from_bytes(bytes: [u8; $size]) -> Self {
-                    Self {
-                        bytes: bytes
-                    }
-                }
-                pub fn to_bytes(&self) -> [u8; $size] {
-                    self.bytes
-                }
-            }
-        }
-    };
+/// Media Access Control address\nUsed in Ethernet, Experimental Ethernet, Token Ring, FDDI and other
+#[derive(Debug, Clone, Copy)]
+pub struct MacAddress {
+    /// $name Address Data Bytes
+    pub bytes: [u8; 6]
 }
-
-#[cfg(feature = "legacy-arp")]
-#[derive(Debug, Clone)]
-pub enum HardwareAddress {
-    Mac(MacAddress),
-    Ax25(Ax25Address),
-    Atm(AtmAddress),
-    Gid(GidAddress),
-    Wwn(WwnAddress),
-    Node(NodeAddress),
-    None
-}
-#[cfg(feature = "legacy-arp")]
-impl HardwareAddress {
+impl MacAddress {
     pub fn new() -> Self {
-        Self::None
+        Self {
+            bytes: [0u8; 6]
+        }
+    }
+    pub fn from_slice(bytes: &[u8]) -> Self {
+        if bytes.len() < 6 {
+            panic!("Bytes len must be $size!");
+        }
+        let mut new_bytes: [u8; 6] = [0; 6];
+        new_bytes.copy_from_slice(bytes);
+        Self {
+            bytes: new_bytes
+        }
+    }
+    pub fn from_bytes(bytes: [u8; 6]) -> Self {
+        Self {
+            bytes: bytes
+        }
+    }
+    pub fn to_bytes(&self) -> [u8; 6] {
+        self.bytes
     }
 }
-
-hardware_address!(Mac, "Media Access Control address\nUsed in Ethernet, Experimental Ethernet, Token Ring, FDDI and other", 6);
-#[cfg(feature = "legacy-arp")]
-hardware_address!(Ax25, "Amateur Radio\nCallsign 6 bytes + SSID 1 byte", 7);
-#[cfg(feature = "legacy-arp")]
-hardware_address!(Atm, "NSAP or ATM address", 20);
-#[cfg(feature = "legacy-arp")]
-hardware_address!(Gid, "GID(IB) address\nGUID 16 bytes + prefix 4 bytes", 20);
-#[cfg(feature = "legacy-arp")]
-hardware_address!(Wwn, "World Wide Name\nUsed in Fibre Channel, ATA, SAS", 8);
-#[cfg(feature = "legacy-arp")]
-hardware_address!(Node, "Just Node ID, used in small networks, like ARCNET or something else", 1);
 
 pub trait Packet {
     fn from_bytes(bytes: &[u8]) -> Self;
